8f61b6acf73129f802ee9884538c324e9aee0599
RobotMermaid <ondinerangel@gmail.com>
2016-11-10 16:37:35 -0800

    add & view entries

--- address_bloc.rb
+++ address_bloc.rb
@@ -1,1 +1,8 @@
-puts "Welcome to Address Bloc"
+require_relative 'controllers/menu_controller'
+
+menu = MenuController.new
+#clears the command line
+system "clear"
+puts "Welcome aboard the Starship AddressBloc!"
+
+menu.main_menu
